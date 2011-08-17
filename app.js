065e35622f62e945476e64d4c14ecd8eb1e33176
Andy Niccolai <andy.niccolai@gmail.com>
2011-08-16 20:47:53 -0600

    completed walkthrough

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
 var app = module.exports = express.createServer();
+
+// Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -21,7 +23,9 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-var articleProvider= new ArticleProvider();
+var articleProvider= new ArticleProvider('localhost', 27017);
+
+// Routes
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
@@ -49,4 +53,25 @@ app.post('/blog/new', function(req, res){
   });
 });
 
+app.get('/blog/:id', function(req, res) {
+  articleProvider.findById(req.params.id, function(error, article) {
+    res.render('blog_show.jade', {locals: {
+      title: article.title,
+      article: article
+    }
+    });
+  });
+});
+
+app.post('/blog/addComment', function(req, res) {
+  articleProvider.addCommentToArticle(req.param('_id'), {
+      person: req.param('person'),
+      comment: req.param('comment'),
+      created_at: new Date()
+    }, function(error, docs) {
+      res.redirect('/blog/'+req.param('_id'))
+    });
+});
+
 app.listen(3000);
+console.log("Express server listenind on port %d in %s mode", app.address().port, app.settings.env);
